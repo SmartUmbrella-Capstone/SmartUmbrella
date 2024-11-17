ffbf838dec2b160e0156239dec4add16cc154a1e
Sudal <ye5779@naver.com>
2024-11-17 17:05:28 +0900

    UserSetting 테이블 수정 및 데이터베이스 설정

--- app/src/main/java/com/example/smartumbrella/DatabaseHelper.java
+++ app/src/main/java/com/example/smartumbrella/DatabaseHelper.java
@@ -7,18 +7,38 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "UserSettings.db";
-    private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_NAME = "settings";
 
-    // Column names
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_UPDATE_ALERT = "updateAlert";
-    public static final String COLUMN_PRIORITY_ALERT = "priorityAlert";
-    public static final String COLUMN_LOCATION_LOG = "locationLog";
-    public static final String COLUMN_DISTANCE = "distance";
-    public static final String COLUMN_VOLUME = "volume";
-    public static final String COLUMN_INTERVAL = "interval";
+    private static final String DATABASE_NAME = "SmartUmbrella.db";
+    private static final int DATABASE_VERSION = 3; // 버전 업데이트
+
+    // UserSetting 테이블
+    private static final String TABLE_USER_SETTING = "UserSetting";
+    private static final String CREATE_TABLE_USER_SETTING = "CREATE TABLE " + TABLE_USER_SETTING + " (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "update_alert INTEGER, " +
+            "priority_alert INTEGER, " +
+            "distance INTEGER, " +
+            "volume INTEGER DEFAULT 50, " + // 기본값 포함
+            "interval TEXT, " +
+            "location_enable INTEGER DEFAULT 0" +
+            ");";
+
+    // LocationLog 테이블
+    private static final String TABLE_LOCATION_LOG = "LocationLog";
+    private static final String CREATE_TABLE_LOCATION_LOG = "CREATE TABLE " + TABLE_LOCATION_LOG + " (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "timestamp TEXT NOT NULL, " +
+            "latitude REAL NOT NULL, " +
+            "longitude REAL NOT NULL" +
+            ");";
+
+    // BatteryStatus 테이블
+    private static final String TABLE_BATTERY_STATUS = "BatteryStatus";
+    private static final String CREATE_TABLE_BATTERY_STATUS = "CREATE TABLE " + TABLE_BATTERY_STATUS + " (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "timestamp TEXT NOT NULL, " +
+            "battery_level INTEGER NOT NULL" +
+            ");";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -26,42 +46,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY, " +
-                COLUMN_UPDATE_ALERT + " INTEGER, " +
-                COLUMN_PRIORITY_ALERT + " INTEGER, " +
-                COLUMN_LOCATION_LOG + " INTEGER, " +
-                COLUMN_DISTANCE + " INTEGER, " +
-                COLUMN_VOLUME + " INTEGER, " +
-                COLUMN_INTERVAL + " TEXT)";
-        db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE_USER_SETTING);
+        db.execSQL(CREATE_TABLE_LOCATION_LOG);
+        db.execSQL(CREATE_TABLE_BATTERY_STATUS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+        if (oldVersion < 2) {
+            // 기존 테이블에 새 컬럼 추가
+            db.execSQL("ALTER TABLE UserSetting ADD COLUMN volume INTEGER DEFAULT 50;");
+        }
+        if (oldVersion < 3) {
+            db.execSQL("ALTER TABLE UserSetting ADD COLUMN location_enable INTEGER DEFAULT 0;");
+        }
     }
 
-    // Save settings to the database
-    public void saveSettings(boolean updateAlert, boolean priorityAlert, boolean locationLog, int distance, int volume, String interval) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_UPDATE_ALERT, updateAlert ? 1 : 0);
-        values.put(COLUMN_PRIORITY_ALERT, priorityAlert ? 1 : 0);
-        values.put(COLUMN_LOCATION_LOG, locationLog ? 1 : 0);
-        values.put(COLUMN_DISTANCE, distance);
-        values.put(COLUMN_VOLUME, volume);
-        values.put(COLUMN_INTERVAL, interval);
-
-        // Insert or update row
-        db.insertWithOnConflict(TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-        db.close();
-    }
-
-    // Retrieve the latest settings from the database
-    public Cursor getSettings() {
+    // DatabaseHelper.java
+    public Cursor getUserSettings() {
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_NAME + " ORDER BY " + COLUMN_ID + " DESC LIMIT 1", null);
+        return db.query("UserSetting", null, null, null, null, null, null);
+    }
+
+    public void saveUserSettings(ContentValues values) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insertWithOnConflict("UserSetting", null, values, SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
     }
 }
